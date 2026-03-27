42cfacfa908601150c64828db25321921f77b488
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2026-03-26 22:31:22 -0300

    Se termina de dar funcionalidad a los botones de script y volver para moverse entre las UIs de combate

--- Assets/Scripts/CombateUI/BotonesCombate.cs
+++ Assets/Scripts/CombateUI/BotonesCombate.cs
@@ -6,11 +6,26 @@ public class BotonesCombate : MonoBehaviour
     public Button BotonEscape;
     public Button BotonObjeto;
     public Button BotonScript;
+    public CombateUIFlowController uiFlowController;
+
+    void Awake() {
+        AutoAssignReferences();
+    }
 
     void Start() {
-        BotonEscape.onClick.AddListener(OnEscape);
-        BotonObjeto.onClick.AddListener(OnObjeto);
-        BotonScript.onClick.AddListener(OnScript);
+        if (uiFlowController == null) {
+            uiFlowController = FindObjectOfType<CombateUIFlowController>();
+        }
+
+        if (BotonEscape != null) BotonEscape.onClick.AddListener(OnEscape);
+        if (BotonObjeto != null) BotonObjeto.onClick.AddListener(OnObjeto);
+        if (BotonScript != null) BotonScript.onClick.AddListener(OnScript);
+    }
+
+    void OnDestroy() {
+        if (BotonEscape != null) BotonEscape.onClick.RemoveListener(OnEscape);
+        if (BotonObjeto != null) BotonObjeto.onClick.RemoveListener(OnObjeto);
+        if (BotonScript != null) BotonScript.onClick.RemoveListener(OnScript);
     }
 
     void OnEscape() {
@@ -24,13 +39,25 @@ public class BotonesCombate : MonoBehaviour
     }
 
     void OnScript() {
-        // Programar menú scripts
         Debug.Log("Script presionado");
+        if (uiFlowController != null) {
+            uiFlowController.OpenScriptConsole();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void AutoAssignReferences() {
+        if (BotonEscape == null) BotonEscape = FindButtonByName("BotonEscape");
+        if (BotonObjeto == null) BotonObjeto = FindButtonByName("BotonObjeto");
+        if (BotonScript == null) BotonScript = FindButtonByName("BotonScript");
+    }
+
+    Button FindButtonByName(string buttonName) {
+        Button[] buttons = FindObjectsOfType<Button>(true);
+        foreach (Button b in buttons) {
+            if (b.name == buttonName) {
+                return b;
+            }
+        }
+        return null;
     }
 }
---
+++ Assets/Scripts/CombateUI/CombateUIFlowController.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombateUIFlowController : MonoBehaviour {
+    [Header("Paneles")]
+    public GameObject combateUI;
+    public GameObject combateUIConsola;
+
+    [Header("Botones (opcional)")]
+    public Button botonScript;
+    public Button botonVolver;
+    public Button botonBorrar;
+    public Button botonEjecutar;
+
+    void Awake() {
+        AutoAssignReferences();
+        SetInitialState();
+    }
+
+    void Start() {
+        if (botonScript != null) {
+            botonScript.onClick.AddListener(OpenScriptConsole);
+        }
+
+        if (botonVolver != null) {
+            botonVolver.onClick.AddListener(BackToCombatUI);
+        }
+
+        if (botonBorrar != null) {
+            botonBorrar.onClick.AddListener(OnBorrar);
+        }
+
+        if (botonEjecutar != null) {
+            botonEjecutar.onClick.AddListener(OnEjecutar);
+        }
+    }
+
+    void OnDestroy() {
+        if (botonScript != null) {
+            botonScript.onClick.RemoveListener(OpenScriptConsole);
+        }
+
+        if (botonVolver != null) {
+            botonVolver.onClick.RemoveListener(BackToCombatUI);
+        }
+
+        if (botonBorrar != null) {
+            botonBorrar.onClick.RemoveListener(OnBorrar);
+        }
+
+        if (botonEjecutar != null) {
+            botonEjecutar.onClick.RemoveListener(OnEjecutar);
+        }
+    }
+
+    public void SetInitialState() {
+        if (combateUI != null) combateUI.SetActive(true);
+        if (combateUIConsola != null) combateUIConsola.SetActive(false);
+    }
+
+    public void OpenScriptConsole() {
+        if (combateUI != null) combateUI.SetActive(false);
+        if (combateUIConsola != null) combateUIConsola.SetActive(true);
+    }
+
+    public void BackToCombatUI() {
+        if (combateUI != null) combateUI.SetActive(true);
+        if (combateUIConsola != null) combateUIConsola.SetActive(false);
+    }
+
+    public void OnBorrar() {
+        Debug.Log("Borrar presionado");
+    }
+
+    public void OnEjecutar() {
+        Debug.Log("Ejecutar presionado");
+    }
+
+    void AutoAssignReferences() {
+        if (combateUI == null) {
+            GameObject go = GameObject.Find("CombateUI");
+            if (go != null) combateUI = go;
+        }
+
+        if (combateUIConsola == null) {
+            GameObject go = GameObject.Find("CombateUI-Consola");
+            if (go != null) combateUIConsola = go;
+        }
+
+        if (botonScript == null) botonScript = FindButtonByName("BotonScript");
+        if (botonVolver == null) botonVolver = FindButtonByName("BotonVolver");
+        if (botonBorrar == null) botonBorrar = FindButtonByName("BotonBorrar");
+        if (botonEjecutar == null) botonEjecutar = FindButtonByName("BotonEjecutar");
+    }
+
+    Button FindButtonByName(string buttonName) {
+        Button[] buttons = FindObjectsOfType<Button>(true);
+        foreach (Button b in buttons) {
+            if (b.name == buttonName) {
+                return b;
+            }
+        }
+
+        return null;
+    }
+}
